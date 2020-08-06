e5aef8a247484e33db770767b734f5d4cc97e7a5
Tyler Huffman <ty.huff24@gmail.com>
2020-08-06 23:47:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialized(word)
+  @word = word
+end
+
+def match[word_array]
+  ana_array = @word.split("")
+  word_array.find_all {|word| worn.split("").sort == ana_array.sort }
+end
