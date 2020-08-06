98389c6d9415e4e237d294f7406775a1b0c903b6
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-06 21:56:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.find_all do |word|
+      is_anagram?(word)
+    end
+  end
+
+  def is_anagram?(word)
+    word.chars.sort == @word.chars.sort
+  end
+
+end
