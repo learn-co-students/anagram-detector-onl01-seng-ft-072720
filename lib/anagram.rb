4b5e7ffea60ef206e04b836480cf9fa834d8198c
Tyler Huffman <ty.huff24@gmail.com>
2020-08-07 11:55:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,14 +15,13 @@ array1 = word.split("").sort {|one,two| one <=> two}
 array2 = word_array.map {|word| word.split("").sort {|one,two| one <=> two}}
 anagram_index = []
 array2.each_with_index do |sorted_array, i|
-  sorted_array == array1 ? anagram_index << i
+  sorted_array == array1 ? anagram_index << i : nil
 end
-winners = nangram_index.map do |i|
+winners = anagram_index.map do |i|
   word_array[i]
 end
 end
 
 # array3 = array2.find_all {|sorted_anagram_array| sorted_anagram_array == array1}
-  binding.pry
-end
+  #binding.pry
 end
