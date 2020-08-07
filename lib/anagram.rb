c45a45dd0466815ea447f058daa6303a3728f392
George Talnagi <gtalnagi@yahoo.com>
2020-08-07 22:53:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    anagrams = []
+    words.each do |new_word|
+      if @word.split("").sort == new_word.split("").sort 
+        anagrams << new_word 
+      end
+    end 
+    anagrams 
+  end 
+end 
