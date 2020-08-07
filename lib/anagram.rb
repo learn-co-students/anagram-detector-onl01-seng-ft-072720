863d9760839d7283dba1900e3404f08b006bd259
Tyler Huffman <ty.huff24@gmail.com>
2020-08-07 11:52:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,9 +13,16 @@ end
 def match(word_array)
 array1 = word.split("").sort {|one,two| one <=> two}
 array2 = word_array.map {|word| word.split("").sort {|one,two| one <=> two}}
-array2.find_all {|sorted_anagram_array| sorted_anagram_array == array1}
-  binding.pry
+anagram_index = []
+array2.each_with_index do |sorted_array, i|
+  sorted_array == array1 ? anagram_index << i
+end
+winners = nangram_index.map do |i|
+  word_array[i]
+end
 end
 
-
+# array3 = array2.find_all {|sorted_anagram_array| sorted_anagram_array == array1}
+  binding.pry
+end
 end
