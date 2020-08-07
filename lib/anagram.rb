a3270e93ef46a472a27e1e3ce5ec3e9618bba4c0
Tyler Huffman <ty.huff24@gmail.com>
2020-08-07 00:12:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,11 +3,14 @@ class Anagram
 
 attr_accessor :word
 
-def initialized(word)
+def initialize(word)
   @word = word
 end
 
-def match[word_array]
-  ana_array = @word.split("")
-  word_array.find_all {|word| worn.split("").sort == ana_array.sort }
+def match(word_array)
+  array1 = @word.split("")
+  array2 = word_array.map {|word| word.split("")}
+  array2.find_all {|word| word.sort == array1}
+  binding.pry
+end
 end
