1e08f9b4ca9df6d7773d13dad2afb1b4fc138fc3
Daniel Gould <dmgould310@gmail.com>
2020-08-07 13:44:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+  def initialize(word)
+  @word = word
+  end 
+  
+  def match(word_array)
+    newarr =[]
+    word_array.each do |word| 
+    if word.split("").sort == @word.split("").sort
+      newarr.push(word)
+    else 
+      end 
+    end 
+    newarr 
+end 
+  
+  
+  
+  
+  
+  
+# binding.pry 
+end 
