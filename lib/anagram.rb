28167cc12e2c7f35e9612fc78fcfb1fc25881edf
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-08 17:08:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :word
+
+
+def initialize(word)
+  @word = word
+end
+
+def match(possible_anagrams)
+  anagrams = []
+  possible_anagrams.each do |maybe_anagram|
+    if  @word.split("").sort == maybe_anagram.split("").sort
+      anagrams << maybe_anagram
+    end
+  end
+  anagrams
+end
+
+
+
+end
